57eed503433477fb3ee3c7e3ab503f920c52703c
sunil <snlkumar447@gmail.com>
2014-10-01 15:00:11 +0530

    error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
      
   protected
     def after_sign_up_path_for(resource)     
-      resource.update_attributes(subdomain: request.subdomain) 
+      resource.update_attributes(subdomain: request.subdomain) unless request.subdomain.blank?
+      sign_in resource
       root_url(:subdomain => resource.name)
     end
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,33 +5,12 @@ class User < ActiveRecord::Base
 
    validates_presence_of :email
   validates_presence_of :encrypted_password
-  
-  ## Recoverable
-  # field :subdomain, :type=>String
-  
-  ## Rememberable
- 
-
-  ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
-
-  ## Token authenticatable
-  # field :authentication_token, :type => String
-  # run 'rake db:mongoid:create_indexes' to create indexes
-  # index({ email: 1 }, { unique: true, background: true })
-  # field :name, :type => String
+  # belongs_to :manager
+  # belongs_to :employe
   validates_presence_of :name
   validates_format_of :name, with: /^[a-z0-9_]+$/, message: 'must be lowercase alphanumerics only'
   validates_length_of :name, maximum: 32, message: 'exceeds maximum of 32 characters'
   validates_exclusion_of :name, in: ['www', 'mail', 'ftp'], message: 'is not available'
-  attr_accessible :name, :email,:subdomain, :password, :password_confirmation, :remember_me, :created_at, :updated_at
+  attr_accessible :name,:employe,:manager, :email,:subdomain, :password, :password_confirmation, :remember_me, :created_at, :updated_at
   
 end
