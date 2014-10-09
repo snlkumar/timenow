17471b53890148e4d7952135f68e3a728f6ac15b
sunil <snlkumar447@gmail.com>
2014-10-09 18:24:26 +0530

    tracker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    @users=User.all
+    unless current_user
+      render "index"
+    else if current_user.employee
+    redirect_to shift_employees_path
+    else
+      render "index"
+    end
+    end
   end
   
   def detail_and_pricing
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,7 @@
 class Employee < ActiveRecord::Base
   attr_accessible :user,:manager_id,:user_attributes,:designation,:first_name,:last_name,:gender,:emp_code,:phone
   has_one :user
+  has_many :shifts
   belongs_to :manager
   accepts_nested_attributes_for :user
 end
