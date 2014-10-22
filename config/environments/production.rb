55c6ccd99af49290d66ba1a6e5984d1f91f89797
sunil <snlkumar447@gmail.com>
2014-10-22 12:53:07 -0400

    gghghhg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ config.logger = Logger.new("/var/www/timenow/log/production.log")
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'yumengine.com' }
+  config.action_mailer.default_url_options = { :host => 'yumengine.com:8080' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
